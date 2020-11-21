ba28a10fcc98128be136096a49f92ff3ec0f88de
Yalovoi <64505068+Yalovoi@users.noreply.github.com>
2020-11-21 10:16:01 -0800

    added solved

--- 6ky/Are they the same.js
+++ 6ky/Are they the same.js
@@ -12,3 +12,10 @@ function comp(array1, array2){
     return true;
   }
 }
+
+function comp(array1, array2) {
+  array1 = array1.map(v => v * v);
+  return array2
+    ? array1.reduce((a, b) => a + b, 0) === array2.reduce((a, b) => a + b, 0)
+    : false;
+}
