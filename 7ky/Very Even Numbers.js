7ff57ee4f3efe3f505ad831101c22536fb77c67c
Yalovoi <64505068+Yalovoi@users.noreply.github.com>
2020-11-20 20:30:35 -0800

    very even numbers

--- 7ky/Very Even Numbers.js
+++ 7ky/Very Even Numbers.js
@@ -17,11 +17,11 @@ function isVeryEvenNumber(n) {
   }
   return str*1%2===0
 }
-
+/*
 function isVeryEvenNumber(n) {
   const numbers = String(n).split('').map(Number)
   const totalSum = numbers.reduce((acc, number) => acc + number, 0)
   while (String(totalSum).length > 1) return isVeryEvenNumber(totalSum)
   return totalSum % 2 === 0
 }
-
+*/
