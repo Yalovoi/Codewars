473afb469248abdafa597483a9f4d9ad88f4a682
Yalovoi <64505068+Yalovoi@users.noreply.github.com>
2020-11-22 14:55:13 -0800

    added link

--- 7ky/Series of integers from m to n.js
+++ 7ky/Series of integers from m to n.js
@@ -1,4 +1,4 @@
-```www.codewars.com/kata/5841f680c5c9b092950001ae/train/javascript```
+https://www.codewars.com/kata/5841f680c5c9b092950001ae/train/javascript
 function generateIntegers(m, n) {
   let newArr = []
   for(let i = m; i <= n;i++){
