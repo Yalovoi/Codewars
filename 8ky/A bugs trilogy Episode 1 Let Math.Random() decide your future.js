605fc471450a9ea6894a42e1ee9136732c31a5f0
Yalovoi <64505068+Yalovoi@users.noreply.github.com>
2020-11-27 09:50:15 -0800

    A bugs trilogy: Episode 1 - 'Let Math.Random'

---
+++ 8ky/A bugs trilogy Episode 1 Let Math.Random() decide your future.js
@@ -0,0 +1,6 @@
+function yourFutureCareer(){
+let career = Math.random()
+if (career <= 0.32) return 'FrontEnd Developer'
+if (career <= 0.65) return 'BackEnd Developer'
+return 'Full-Stack Developer'
+   }
