eb11b4bb60984a0289e55b13917f7cc79a329a38
Yalovoi <64505068+Yalovoi@users.noreply.github.com>
2020-11-28 22:31:56 -0800

    //https://www.codewars.com/kata/577a98a6ae28071780000989/solutions/javascript

--- 8ky/Find Maximum and Minimum Values of a List.js
+++ 8ky/Find Maximum and Minimum Values of a List.js
@@ -1,3 +1,4 @@
+//https://www.codewars.com/kata/577a98a6ae28071780000989/solutions/javascript
 /*
 Description:
 Your task is to make two functions, max and min (maximum and minimum in PHP) that take a(n) array/vector of integers list as input and outputs, respectively, the largest and lowest number in that array/vector.
