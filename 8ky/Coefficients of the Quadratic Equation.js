c236bff5598217483eb15aa966f55287be6c96e0
Yalovoi <64505068+Yalovoi@users.noreply.github.com>
2020-11-28 22:36:10 -0800

    //

--- 8ky/Coefficients of the Quadratic Equation.js
+++ 8ky/Coefficients of the Quadratic Equation.js
@@ -1,3 +1,4 @@
+//https://www.codewars.com/kata/5d59576768ba810001f1f8d6/train/javascript
 /*
 Description:
 In this Kata you are expected to find the coefficients of quadratic equation of the given two roots (x1 and x2).
