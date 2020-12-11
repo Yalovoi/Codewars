69e04b8619301f47adb50557ad25cb2d3fa3b5b8
Yalovoi <64505068+Yalovoi@users.noreply.github.com>
2020-12-10 23:03:12 -0800

    //https://www.codewars.com/kata/5a6663e9fd56cb5ab800008b/train/javascript

---
+++ 8ky/Cat years, Dog years.js
@@ -0,0 +1,11 @@
+let humanYearsCatYearsDogYears = function(humanYears) {
+  const catYears = humanYears === 1 ? 15 :
+                   humanYears === 2 ? 15 + 9 :
+                   15 + 9 + ((humanYears - 2) * 4);
+
+  const dogYears = humanYears === 1 ? 15 :
+                   humanYears === 2 ? 15 + 9 :
+                   15 + 9 + ((humanYears - 2) * 5);
+
+  return [humanYears,catYears,dogYears];
+}
