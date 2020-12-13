d7f76f570d63abef6edd6e713e5b85325d76304f
Yalovoi <64505068+Yalovoi@users.noreply.github.com>
2020-12-13 10:34:05 -0800

    //https://www.codewars.com/kata/5733f948d780e27df6000e33/train/javascript

--- 8ky/Training JS #34 methods of Math---pow() sqrt() and cbrt().js
+++ 8ky/Training JS #34 methods of Math---pow() sqrt() and cbrt().js
@@ -1,3 +1,7 @@
 function cutCube(volume,n){
 return Math.cbrt(volume / n) % 1 === 0 && Math.cbrt(n) % 1 === 0 ? true : false;
   }
+
+   function cutCube(volume,n){
+   return Math.cbrt(volume) % 1 == 0 && Math.cbrt(volume / n) % 1 == 0;
+  }
